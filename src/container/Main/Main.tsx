dce145ac3c064c4b00f358755208dc8aba5f2cd8
Denis <marvelick753@gmail.com>
2023-02-18 21:01:28 +0200

    add slider

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,6 +1,11 @@
 import './Main.scss'
+import CarouselBox from '../../components/CarouselBox/CarouselBox.js'
 type Props = {}
 const Main = (props: Props) => {
-    return <div className="content">Main</div>
+    return (
+        <div className="content">
+            <CarouselBox />
+        </div>
+    )
 }
 export default Main
